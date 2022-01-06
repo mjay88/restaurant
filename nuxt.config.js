a2977f435d6dc75fd954db511ed2a12c66b08275
Michael W Jarrett <67067893+mjay88@users.noreply.github.com>
2022-01-05 21:48:30 -0600

    Added head method globally and locally, updated font style

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'restaurant',
+        title: "Smokey Joes' Cafe",
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
+            { hid: 'description', name: "Smokey Joes' Cafe", content: 'a restaurant' },
             { name: 'format-detection', content: 'telephone=no' }
         ],
         link: [
